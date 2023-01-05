4dcb02bc6038e0937b073bd2b2b76f15e95f3a28
Ghaffar Daher <ghaffardaher92@gmail.com>
2023-01-05 08:14:35 +0300

    scaffold

---
+++ AppDbContext/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace AppDbContext.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            CategoryProduct = new HashSet<CategoryProduct>();
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public virtual ICollection<CategoryProduct> CategoryProduct { get; set; }
+    }
+}
